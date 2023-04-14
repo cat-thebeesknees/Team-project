3ff4e838bedea0f7ea0802fda0c319f00b4fbf1e
Catherine Hilton <catherine.hilton1786@gmail.com>
2023-04-14 18:11:46 -0400

    made the link for the contact form send the user to another page.

--- javascript.js
+++ javascript.js
@@ -20,23 +20,23 @@ function openPage(evt, pageName) {
     
 }
 
-const form = document.querySelector("#form")
-const submitButton = document.querySelector("#submit")
-const scriptURL = 'https://httpbin.org/post'
+// const form = document.querySelector("#form")
+// const submitButton = document.querySelector("#submit")
+// const scriptURL = 'sucessful.html'
 
-form.addEventListener('submit', e => {
-  submitButton.disabled = true
-  e.preventDefault()
-  let requestBody = new FormData(form)
-  fetch(scriptURL, { method: 'POST', body: requestBody})
-    .then(response => {
-       alert('Success!', response)
-       submitButton.disabled = false
-      })
-    .catch(error => {
-    alert('Error!', error.message)
-      submitButton.disabled = false
+// form.addEventListener('submit', e => {
+//   submitButton.disabled = true
+//   e.preventDefault()
+//   let requestBody = new FormData(form)
+//   fetch(scriptURL, { method: 'POST', body: requestBody})
+//     .then(response => {
+//        alert('Success!', response)
+//        submitButton.disabled = false
+//       })
+//     .catch(error => {
+//     alert('Error!', error.message)
+//       submitButton.disabled = false
 
-    }
-    )
-})
+//     }
+//     )
+// })
